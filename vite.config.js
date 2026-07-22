adb385cad475f661534d0dbd847ab298b728e81d
XavierXhz <xavier@users.noreply.github.com>
2026-07-22 17:02:10 +0800

    fix: vite 默认端口 5174→8899, strictPort→false

--- vite.config.js
+++ vite.config.js
@@ -144,8 +144,8 @@ export default defineConfig({
     },
     server: {
         host: true,
-        port: 5174,
-        strictPort: true,
+        port: 8899,
+        strictPort: false,
         open: false,
         fs: {
             allow: [__dirname]
